16912eca81c4e1675b1923fbcc27e7e8283f4a2b
danilec13 <danila_elshin@hotmail.com>
2019-06-15 02:40:07 +0300

    nasa articles

--- rss_articles.js
+++ rss_articles.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableHighlight, StyleSheet } from 'react-native';
+import { View, Text, TouchableHighlight, StyleSheet, Image } from 'react-native';
 //https://www.npmjs.com/package/react-native-rss-parser
 import * as rssParser from 'react-native-rss-parser';
 import i18n from 'sources/i18n';
@@ -19,25 +19,27 @@ class FetchCars extends Component {
       loadingButtonActive: false,
       loadingButtonVisible: styles.visible,
       visibleBody: styles.hidden,
-      data: ''
+      items: []
    }
 
    loadCars(){
-      var uri = 'https://e00-marca.uecdn.es/rss/motor/modelos-coches.xml';
-      //var uri = 'http://www.nasa.gov/rss/dyn/breaking_news.rss';
+      //var uri = 'https://e00-marca.uecdn.es/rss/motor/modelos-coches.xml';
+      var uri = 'http://www.nasa.gov/rss/dyn/breaking_news.rss';
       fetch(uri,{
          method: 'GET'
       })
       .then((response) => response.text())
       .then((responseData) => rssParser.parse(responseData))
       .then((rss) => {
-         console.log(rss.title);
+         //console.log(rss.title);
          console.log(rss.items[0]);
-         console.log("" + rss.items[0].image);
+         console.log("" + rss.items[0].title);
+         console.log("" + rss.items[0].description);
+         console.log("" + rss.items[0].enclosures[0].url);
          console.log(rss.items.length);
          //console.log(this);
          this.setState({
-            data: rss,
+            items: rss.items,
             loadingButtonActive: false,
             loadingButtonVisible: styles.hidden,
             visibleBody: styles.visible
@@ -64,9 +66,31 @@ class FetchCars extends Component {
                   <Text >{i18n.t('loadBtn')}</Text>
                </View>
             </TouchableHighlight>
-            <Text style={this.state.visibleBody}>
+            {/*<WebView style={{height: 40, backgroundColor: "red"}}
+            originWhitelist={['*']}
+            source={{html: '<h1>Hello world</h1>'}}
+            />*/}
+            {/*<Text style={this.state.visibleBody}>
                {JSON.stringify(this.state.data)}
-            </Text>
+            </Text>*/
+            }
+            {
+               this.state.items.map((item, index) => (
+                  <View key = {index} >
+                     <Text>{item.title}</Text>
+                     <Text>{item.description}</Text>
+                     {item.enclosures.map((item, index) => (
+                        <View key = {index} >
+                           <Image
+                              style={{width: 50, height: 50}}
+                              source={{uri: item.url}}
+                           />
+                           <Text>{item.url}</Text>
+                        </View>
+                     ))}
+                  </View>
+               ))/**/
+            }
          </View>
       )
    }
